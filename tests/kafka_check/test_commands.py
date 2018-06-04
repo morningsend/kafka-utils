d3e5d53cdde07f3a57f1a38cce106b1939df6741
Zaiyang Li <zaiyangl@yelp.com>
2018-06-04 18:57:51 +0100

    fix unit test error

--- tests/kafka_check/test_commands.py
+++ tests/kafka_check/test_commands.py
@@ -6,7 +6,7 @@ def test_is_first_broker_empty_broker_list_return_false():
     broker_ids = []
     assert not(is_first_broker(broker_ids, broker_id))
 
-def test_is_first_broker_returns_false_if_broker_id_not_exist_in_broker_id_list:
+def test_is_first_broker_returns_false_if_broker_id_not_exist_in_broker_id_list():
     broker_id = 10
     broker_ids = [2, 3]
     assert not(is_first_broker(broker_ids, broker_id))
